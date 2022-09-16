07211de83395c4c3add232e61f744a68854a81ed
JashoBell <JoshuaDB@gmail.com>
2022-09-15 17:10:50 -0700

    Saving/naming
    - Can either overwrite original file or add a basename containing the rotation angle and any flips that occurred

--- scripts/rotate_output.py
+++ scripts/rotate_output.py
@@ -1,5 +1,6 @@
 import modules.scripts as scripts
 import gradio as gr
+import os
 
 from modules import images
 from modules.processing import process_images, Processed
@@ -20,11 +21,12 @@ class Script(scripts.Script):
         angle = gr.Slider(minimum=0.0, maximum=360.0, step=1, value=0, label="Angle")
         hflip = gr.Checkbox(False, label="Horizontal flip")
         vflip = gr.Checkbox(False, label="Vertical flip")
+        overwrite = gr.Checkbox(False, label="Overwrite existing files")
 
-        return [angle, hflip, vflip]
+        return [angle, hflip, vflip, overwrite]
 
-    def run(self, p, angle, hflip, vflip):
-        print("Rotating output")
+    def run(self, p, angle, hflip, vflip, overwrite):
+        
         # function which takes an image, an angle and two booleans indicating horizontal and vertical flips, then returns the image rotated and flipped accordingly
         def rotate_and_flip(im, angle, hflip, vflip):
             raf = im
@@ -37,9 +39,18 @@ class Script(scripts.Script):
             return raf
 
         proc = process_images(p)
+        basename = ""
+        if(not overwrite):
+            if angle != 0:
+                basename += "rotated_" + str(angle)
+            if hflip:
+                basename += "_hflip"
+            if vflip:
+                basename += "_vflip"
         
         # rotate and flip each image in the processed images
         for i in range(len(proc.images)):
             proc.images[i] = rotate_and_flip(proc.images[i], angle, hflip, vflip)
+            images.save_image(proc.images[i], p.outpath_samples, basename, proc.seed + i, proc.prompt, opts.samples_format, info= proc.info, p=p)
             
         return proc
